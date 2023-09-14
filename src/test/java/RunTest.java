1ecd657be44be36f951154d1526a3d782e04c31e
TareevND <nik.tareev.00@mail.ru>
2023-09-14 13:13:42 +0300

    added main method with open website

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,27 +1,35 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import Hooks.DriverSetups;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class RunTest {
+public class RunTest extends DriverSetups {
+
+    public void main(){
+        open("https://demoqa.com/automation-practice-form");
+    }
 
     int number = 6;
 
-    @Before
-    public void beforeTest(){
-        System.out.println("This method before Test");
+    @BeforeAll
+    public static void beforeTests(){
+        System.out.println("This method before Tests");
     }
 
-    @After
-    public void afterTest(){
-        System.out.println("This method after Test");
+    @AfterAll
+    public static void afterTests(){
+        System.out.println("This method after Tests");
     }
 
     @Test
     public void test1(){
+        main();
         System.out.println("Test 1");
         boolean flag = number == PseudoMethods.pseudoMethod(3);
         assertTrue("Сумма значений не удовлетворяет условию",flag);
@@ -29,6 +37,7 @@ public class RunTest {
 
     @Test
     public void test2(){
+        main();
         System.out.println("Test 2");
         assertEquals("Сумма значений не удовлетворяет условию",number, new PseudoMethods().pseudoMethod2(4));
     }
@@ -36,6 +45,7 @@ public class RunTest {
     //Тест, если к примеру ошибка отловленная ассертом, не должна крашить аппликейшн
     @Test
     public void test3(){
+        main();
         System.out.println("Test 3");
         try {
             assertEquals(number, new PseudoMethods().pseudoMethod2(4));
