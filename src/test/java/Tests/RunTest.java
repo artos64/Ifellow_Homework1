b20cd56879b2256825c809812f73e56ef7ad0bf3
TareevND <nik.tareev.00@mail.ru>
2023-09-28 00:37:01 +0300

    deleted unused code

--- src/test/java/Tests/RunTest.java
+++ src/test/java/Tests/RunTest.java
@@ -1,7 +1,6 @@
 package Tests;
 
 import Hooks.DriverSetups;
-import PageObject.BaseSteps.*;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,43 +15,10 @@ import org.junit.runner.RunWith;
         tags = "@Test"
 )
 public class RunTest extends DriverSetups {
-    public final StartPageSteps startPageSteps = new StartPageSteps();
-    public final SystemDashboardSteps systemDashboardSteps = new SystemDashboardSteps();
-    public final OpenTasksSteps openTasksSteps = new OpenTasksSteps();
-    public final TaskSeleniumSteps taskseleniumsteps = new TaskSeleniumSteps();
-    public final WindowOfCreationSteps windowOfCreationSteps = new WindowOfCreationSteps();
-    public final ChangeStatusSteps changeStatusSteps = new ChangeStatusSteps();
-
 
     @DisplayName("Тест от Ifellow по EduJira")
     @Test
     public void checkCreateTaskTest(){
-//        //авторизация в джира
-//        startPageSteps.openUrl();
-//        startPageSteps.enterLoginAndPassword();
-        //открытие проекта Test
-        //systemDashboardSteps.openProject();
 
-        //получение счетчика кол-ва задач
-        //int hashText = openTasksSteps.memberTask();
-
-        //отрытие задачи testSelenium
-        //openTasksSteps.findTask("TEST-28409");
-        //проверка версии и статуса задачи testSelenium
-
-        //taskseleniumsteps.checkVersionStatus();
-
-        //создаем новую задачу, наименование генерируем
-        //String nameTask ="Тема" + UUID.randomUUID().toString().substring(0,10);
-//        windowOfCreationSteps.createTask(nameTask);
-//        //переход к в проект Test для сравнения счетчика
-//        //systemDashboardSteps.openProject();
-//        //проверяем через хеш-таблицу отличие в значении счетчика
-//        //openTasksSteps.checkNumberOfTasks(hashText);
-//        //открываем созданную задачу
-//        openTasksSteps.findTask(nameTask);
-        //проверяем изменение статусов (двигаем по статусам) и их соответствии на общей форме, т.е привязку кнопок к статусам
-        //через конструкцию while
-        changeStatusSteps.checkAndChangeStatus();
     }
 }
